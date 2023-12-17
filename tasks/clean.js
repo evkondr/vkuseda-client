12f9a52db0dba6bd9b1a757df81fd6f9f3c80e7d
evkondr <kondratiev.evg@gmail.com>
2023-12-17 18:14:03 +0300

    init: init react template

--- tasks/clean.js
+++
@@ -1,9 +0,0 @@
-const { src } = require('gulp');
-const cleanDir = require('gulp-clean');
-const { path } = require('../config');
-
-const clean = () => {
-  return src(`${path.defaultDest}/*`).pipe(cleanDir());
-}
-
-module.exports = clean;
--- tasks/scripts.js
+++
@@ -1,8 +0,0 @@
-const { src, dest } = require('gulp');
-const { path } = require('../config');
-
-const scripts = () => {
-  return src(path.scripts.src)
-  .pipe(dest(path.scripts.dest))
-}
-module.exports = scripts;
