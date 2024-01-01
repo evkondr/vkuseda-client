579a66042e865f5ba258f9215968943f483c40fc
evkondr <kondratiev.evg@gmail.com>
2024-01-02 00:05:19 +0300

    feat: add mobile filter

--- src/Components/MenuFilter/MenuFilter.tsx
+++ src/Components/MenuFilter/MenuFilter.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Box, Chip, Stack, Divider,
+  Box, Chip, Stack, Divider, Button, Menu, MenuItem, ChipOwnProps,
 } from '@mui/material';
 import { TCategory } from '../../types';
+import './style.scss';
 
 type TMenuFilterProps = {
   categories: TCategory[],
@@ -10,14 +11,82 @@ type TMenuFilterProps = {
   handleFilterButton: (name:string) => void
 }
 const MenuFilter = ({ categories, handleFilterButton }:TMenuFilterProps) => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const [activeFilter, setActiveFilter] = useState<number>(0);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
   return (
     <Box>
-      <Stack direction="row" spacing={1} marginBottom={1} alignItems="center" flexWrap="wrap">
+      {/* Filter for large screen */}
+      <Stack className="filter-lg" spacing={1}>
         <p>Фильтр:</p>
-        {categories.map((category:TCategory) => (
-          <Chip style={{ marginBottom: '5px' }} label={category.name} variant="outlined" color="success" key={category.id} onClick={() => handleFilterButton(category.name)} />
-        ))}
+        {categories.map((category:TCategory, index:number) => {
+          let variant:ChipOwnProps['variant'] = 'outlined';
+          if (activeFilter === index) {
+            variant = 'filled';
+          }
+          return (
+            <Chip
+              style={{ marginBottom: '5px' }}
+              label={category.name}
+              variant={variant}
+              color="success"
+              key={category.id}
+              onClick={() => {
+                handleFilterButton(category.name);
+                setActiveFilter(index);
+              }}
+            />
+          );
+        })}
       </Stack>
+      {/* Filter for mobile */}
+      <Box className="filter-mobile">
+        <Button
+          id="demo-positioned-button"
+          aria-controls={open ? 'demo-positioned-menu' : undefined}
+          aria-haspopup="true"
+          aria-expanded={open ? 'true' : undefined}
+          onClick={handleClick}
+          variant="contained"
+          color="success"
+        >
+          Фильтр
+        </Button>
+        <Menu
+          id="demo-positioned-menu"
+          aria-labelledby="demo-positioned-button"
+          anchorEl={anchorEl}
+          open={open}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+        >
+          {categories.map((category:TCategory) => (
+            <MenuItem
+              key={category.id}
+              onClick={() => {
+                handleFilterButton(category.name);
+                setAnchorEl(null);
+              }}
+            >
+              {category.name}
+
+            </MenuItem>
+          ))}
+        </Menu>
+      </Box>
       <Divider />
     </Box>
   );
