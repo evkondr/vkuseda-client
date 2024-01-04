ffe3f2cfd64566fa921fd5d77bd0537662cd2417
evkondr <kondratiev.evg@gmail.com>
2024-01-05 01:06:52 +0300

    feat: add NotFoundPage

--- src/router.tsx
+++ src/router.tsx
@@ -4,11 +4,13 @@ import LandingPage from './Components/Pages/LandingPage';
 import MainPage from './Components/Pages/MainPage';
 import MenuPage from './Components/Pages/MenuPage';
 import CartPage from './Components/Pages/CartPage';
+import PageNotFound from './Components/Pages/PageNotFound';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <LandingPage />,
+    errorElement: <PageNotFound />,
   },
   {
     path: '/main',
