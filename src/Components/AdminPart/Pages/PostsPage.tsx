3b00cc47d644035955dae6f50ef3787caf44cb10
evkondr <kondratiev.evg@gmail.com>
2024-01-17 18:37:48 +0300

    feat: add post-card structure and styles

--- src/Components/AdminPart/Pages/PostsPage.tsx
+++ src/Components/AdminPart/Pages/PostsPage.tsx
@@ -7,7 +7,7 @@ import PostCard from '../PostCard/PostCard';
 const PostsPage = () => {
   return (
     <AdminContainer headerText="Записи" buttonHandler={() => console.log('handler')}>
-      <Box padding={1}>
+      <Box display="flex" flexDirection="column" padding={3} rowGap={2}>
         {menuItems.map((item) => <PostCard menuItem={item} />)}
       </Box>
     </AdminContainer>
--- src/Components/AdminPart/PostCard/PostCard.tsx
+++ src/Components/AdminPart/PostCard/PostCard.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import {
-  Card, CardContent, CardMedia, Typography,
+  Card, CardContent, CardMedia, Typography, Box, IconButton,
 } from '@mui/material';
+import EditNoteIcon from '@mui/icons-material/EditNote';
+import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { TMenuItem } from '../../../types';
+import './index.scss';
 
 type PostCardProps ={
     menuItem: TMenuItem
@@ -13,16 +16,35 @@ const PostCard = ({ menuItem }:PostCardProps) => {
     name, ingredients, image, imageAlt,
   } = menuItem;
   return (
-    <Card>
-      <CardContent sx={{ display: 'flex' }}>
+    <Card className="post-card">
+      {/* Card image */}
+      <Box className="post-card__image">
+        <CardMedia component="img" image={image} alt={imageAlt} />
+      </Box>
+      {/* Card content */}
+      <CardContent className="post-card__content">
         <Typography variant="h5">
           {name}
         </Typography>
-        <Typography>
+        <Box>
+          {/* Card description */}
+          <Typography variant="body2">
+            Описание:
+          </Typography>
+        </Box>
+        <Typography variant="body1">
           {ingredients}
         </Typography>
+        <Box className="post-card__buttons">
+          {/* Card control */}
+          <IconButton aria-label="Edit">
+            <EditNoteIcon />
+          </IconButton>
+          <IconButton aria-label="Delete">
+            <DeleteOutlineIcon />
+          </IconButton>
+        </Box>
       </CardContent>
-      <CardMedia width="150px" component="img" image={image} alt={imageAlt} />
     </Card>
   );
 };
