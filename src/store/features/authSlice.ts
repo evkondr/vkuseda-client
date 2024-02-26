e357d86ec3bd5532612f400d852c5cd9c6ad8dd5
evkondr <kondratiev.evg@gmail.com>
2024-02-26 23:22:30 +0300

    feat: add TokenManager, Login logic

--- src/store/features/authSlice.ts
+++ src/store/features/authSlice.ts
@@ -2,6 +2,7 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import loginUser from '../thunks/authThunks';
+import TokenManager from '../../utils/tokenManager';
 
 type authState = {
   token: null | string,
@@ -19,13 +20,17 @@ const authSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder.addCase(loginUser.fulfilled, (state, action) => {
-      console.log(action.payload);
+      state.token = action.payload.result.token;
+      TokenManager.save(state.token);
+      toast.success(action.payload.message);
+      state.loading = false;
     });
-    builder.addCase(loginUser.pending, () => {
-      console.log('loading');
+    builder.addCase(loginUser.pending, (state) => {
+      state.loading = true;
     });
     builder.addCase(loginUser.rejected, (state, action) => {
       state.error = action.payload as string;
+      state.loading = false;
       toast.error(action.payload);
     });
   },
--- src/store/thunks/authThunks.ts
+++ src/store/thunks/authThunks.ts
@@ -3,30 +3,29 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { isAxiosError } from 'axios';
 import { userLogin } from '../../http/authAPI';
 
-// type TLoginResponse = {
-//   message: string,
-//   result: {
-//     token: string
-//   }
-// }
-const loginUser = createAsyncThunk<any, {login:string, password:string}, {
+type TLoginResponse = {
+  message: string,
+  result: {
+    token: string
+  }
+}
+const loginUser = createAsyncThunk<
+TLoginResponse,
+{login:string, password:string},
+{
   rejectValue:string
-}>(
-  'auth/login',
-  // if you type your function argument here
-  async (loginPayload, { rejectWithValue }) => {
-    try {
-      const response = await userLogin(loginPayload.login, loginPayload.password);
-      return response;
-    } catch (error: unknown) {
-      if (isAxiosError(error)) {
-        return rejectWithValue(error.message);
-      }
-      if (error instanceof Error) {
-        return rejectWithValue(error.message);
-      }
-      return rejectWithValue('Неизвестная ошибка');
+}>('auth/login', async (loginPayload, { rejectWithValue }) => {
+  try {
+    const response = await userLogin(loginPayload.login, loginPayload.password);
+    return response.data;
+  } catch (error: unknown) {
+    if (isAxiosError(error)) {
+      return rejectWithValue(error.response?.data.message);
     }
-  },
-);
+    if (error instanceof Error) {
+      return rejectWithValue(error.message);
+    }
+    return rejectWithValue('Неизвестная ошибка');
+  }
+});
 export default loginUser;
