5ba4880900ebd33153ac90783079fd41ae905dc6
evkondr <kondratiev.evg@gmail.com>
2024-03-10 19:05:04 +0300

    feat: add checkAuth service and refactor AuthService class

---
+++ src/http/authService.ts
@@ -0,0 +1,27 @@
+import { httpRequest, authHttpRequest } from '.';
+
+const URL = '/auth';
+class AuthService {
+  static async userLogin(login:string, password:string) {
+    const response = httpRequest.post(`${URL}/login`, {
+      login,
+      password,
+    });
+    return response;
+  }
+
+  static async registration(login:string, email: string, password:string) {
+    const response = httpRequest.post(`${URL}/registration`, {
+      login,
+      email,
+      password,
+    });
+    return response;
+  }
+
+  static async chechAuth() {
+    const response = await authHttpRequest.get(`${URL}/check`);
+    return response;
+  }
+}
+export default AuthService;
