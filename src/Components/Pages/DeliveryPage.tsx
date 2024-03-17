63d85fc49aa223ae3ad7e1f3340e10296d191a60
evkondr <kondratiev.evg@gmail.com>
2024-03-17 18:58:30 +0300

    feat: add delivery page

--- src/Components/Pages/DeliveryPage.tsx
+++ src/Components/Pages/DeliveryPage.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import { Box, Container } from '@mui/material';
-import NotImplemented from '../NotImplemented/NotImplemented';
+import { Box, Container, Typography } from '@mui/material';
+import SectionHeader from '../SectionHeader/SectionHeader';
+import './index.scss';
 
 const DeliveryPage = () => {
   return (
-    <Box component="section">
+    <Box component="section" className="delivery-page">
       <Container maxWidth="lg">
-        <NotImplemented />
+        <SectionHeader>
+          Доставка
+        </SectionHeader>
+        <Typography component="b">
+          Районы:
+        </Typography>
+        <Typography component="p">
+          Мы организовываем доставку по всем районам Нижнего Новгорода.
+        </Typography>
+        <Typography component="b">
+          Время:
+        </Typography>
+        <Typography>
+          Доставка осуществляется с 11:00 до 15:00
+          Для своевременной доставки просим вас делать заказ до 10:30.
+        </Typography>
+        <Typography component="b">
+          Сумма заказ:
+        </Typography>
+        <Typography>
+          Мы ориентированы на офисы и предприятия, в связи с чем,
+          минимальная сумма заказа 1000 рублей
+        </Typography>
       </Container>
     </Box>
   );
